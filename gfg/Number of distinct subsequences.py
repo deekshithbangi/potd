8eff7a9a0af55450fa8d3430dcee2ba30cc5a6a7
Deekshith Bangi <20eg106205@anurag.edu.in>
2023-10-22 21:27:45 +0530

    completed pending

--- gfg/Number of distinct subsequences.py
+++ gfg/Number of distinct subsequences.py
@@ -19,3 +19,31 @@ Output:
 Explanation: 
 The four distinct subsequences are "", "g", "gg", "ggg".
 '''
+def distinctSubsequences(self, s):
+		# code here 
+        dp = [0 for i in range(len(s) + 1)] 
+        dp[0] = 1 
+        i = 1 
+        d = dict() 
+        while i < len(dp): 
+            dp[i] = (dp[i-1] *2) % (10**9 + 7)
+            character = s[i-1] 
+            
+            if character in d:
+                dp[i] -= dp[d[character] - 1]
+                
+            d[character] = i
+            i+=1    
+        return dp[-1] % (10 ** 9 + 7) 
+
+'''
+create a dp with the length(s) + 1 
+set dp[0] to 1 
+create a dict 
+loop through the string 
+set the current_value as previous * 2 
+if current character is already in dict then subtract the value of dp[ index of character - 1] 
+add character into dict with the i as value 
+
+use (10 ** 9 + 7) -- as answer will be too long and as per question
+'''

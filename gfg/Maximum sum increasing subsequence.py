6c968ab6c8ee5b159d97c12cd1d732a1a105d2e3
Deekshith Bangi <20eg106205@anurag.edu.in>
2023-10-24 23:51:32 +0530

    pending codes

---
+++ gfg/Maximum sum increasing subsequence.py
@@ -0,0 +1,20 @@
+'''
+Given an array of n positive integers. Find the sum of the maximum sum subsequence of the given array such that the integers in the subsequence are sorted in strictly increasing order i.e. a strictly increasing subsequence. 
+
+Example 1:
+
+Input: 
+N = 5, arr[] = {1, 101, 2, 3, 100} 
+Output: 
+106
+Explanation:
+The maximum sum of a increasing sequence is obtained from {1, 2, 3, 100},
+Example 2:
+
+Input: 
+N = 4, arr[] = {4, 1, 2, 3}
+Output: 
+6
+Explanation:
+The maximum sum of a increasing sequence is obtained from {1, 2, 3}.
+'''
